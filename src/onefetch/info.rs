46dea2502ebcf6bb32319afd46df591f324e4bcc
o2sh <ossama-hjaji@live.fr>
2020-10-21 16:03:19 +0200

    merge declaration of logo_lines with test on ascii_input

--- src/onefetch/info.rs
+++ src/onefetch/info.rs
@@ -270,13 +270,11 @@ impl std::fmt::Display for Info {
                 panic!("No image backend found")
             }
         } else {
-            let mut logo_lines;
-
-            if let Some(custom_ascii) = &self.config.ascii_input {
-                logo_lines = AsciiArt::new(custom_ascii, Vec::new(), !self.config.no_bold);
+            let mut logo_lines = if let Some(custom_ascii) = &self.config.ascii_input {
+                AsciiArt::new(custom_ascii, Vec::new(), !self.config.no_bold)
             } else {
-                logo_lines = AsciiArt::new(self.get_ascii(), self.colors(), !self.config.no_bold);
-            }
+                AsciiArt::new(self.get_ascii(), self.colors(), !self.config.no_bold)
+            };
 
             loop {
                 match (logo_lines.next(), info_lines.next()) {
