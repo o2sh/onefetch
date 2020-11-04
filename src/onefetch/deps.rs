deefcb53631635fa76d0f331d852b5c18ba3a487
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-04 15:22:26 -0500

    feat: add support for yarn
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/onefetch/deps.rs
+++ src/onefetch/deps.rs
@@ -14,7 +14,13 @@ impl std::fmt::Display for PackageManager {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
             PackageManager::GoModules => write!(f, "Go Modules"),
-            PackageManager::Npm => write!(f, "Npm"),
+            PackageManager::Npm => {
+                if std::path::Path::new("./yarn.lock").exists() {
+                    write!(f, "Yarn")
+                } else {
+                    write!(f, "Npm")
+                }
+            }
             PackageManager::Pip => write!(f, "Pip"),
         }
     }
