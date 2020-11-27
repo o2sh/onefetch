72e36822bc00095c1eb7a19976eb770a2ef86642
o2sh <ossama-hjaji@live.fr>
2020-11-27 19:21:42 +0100

    move check for valid Git repo further down #329

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,7 @@ fn run() -> Result<()> {
     #[cfg(windows)]
     let _ = ansi_term::enable_ansi_support();
 
-    if !cli_utils::is_git_installed() {
-        return Err("git failed to execute!".into());
-    }
-
     let config = Cli::new()?;
-
-    if !repo::is_valid(&config.repo_path)? {
-        return Err("please run onefetch inside of a non-bare git repository".into());
-    }
 
     if config.print_languages {
         return cli_utils::print_supported_languages();
@@ -28,6 +20,14 @@ fn run() -> Result<()> {
 
     if config.print_package_managers {
         return cli_utils::print_supported_package_managers();
+    }
+
+    if !cli_utils::is_git_installed() {
+        return Err("git failed to execute!".into());
+    }
+
+    if !repo::is_valid(&config.repo_path)? {
+        return Err("please run onefetch inside of a non-bare git repository".into());
     }
 
     let info = info::Info::new(config)?;
