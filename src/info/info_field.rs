384e61a2d7730a66f1c9c251c5f61e8d86d737d8
Spenser Black <spenserblack01@gmail.com>
2022-06-15 12:58:20 -0400

    Remove unused function

--- src/info/info_field.rs
+++ src/info/info_field.rs
@@ -46,38 +46,6 @@ pub struct InfoFieldOff {
 }
 
 impl InfoFieldOff {
-    #[deprecated]
-    pub fn new(fields_to_hide: Vec<String>) -> Result<Self> {
-        let mut info_field_off = InfoFieldOff {
-            ..Default::default()
-        };
-
-        for field in fields_to_hide.iter() {
-            let item = InfoField::from_str(field.to_lowercase().as_str())?;
-
-            match item {
-                InfoField::GitInfo => info_field_off.git_info = true,
-                InfoField::Project => info_field_off.project = true,
-                InfoField::Head => info_field_off.head = true,
-                InfoField::Pending => info_field_off.pending = true,
-                InfoField::Version => info_field_off.version = true,
-                InfoField::Created => info_field_off.created = true,
-                InfoField::Languages => info_field_off.languages = true,
-                InfoField::Dependencies => info_field_off.dependencies = true,
-                InfoField::Authors => info_field_off.authors = true,
-                InfoField::LastChange => info_field_off.last_change = true,
-                InfoField::Contributors => info_field_off.contributors = true,
-                InfoField::Repo => info_field_off.repo = true,
-                InfoField::Commits => info_field_off.commits = true,
-                InfoField::LinesOfCode => info_field_off.lines_of_code = true,
-                InfoField::Size => info_field_off.size = true,
-                InfoField::License => info_field_off.license = true,
-            }
-        }
-
-        Ok(info_field_off)
-    }
-
     pub fn from_info_fields(fields_to_hide: &[InfoField]) -> Self {
         // TODO Implement using From trait instead?
         let mut info_field_off = InfoFieldOff {
