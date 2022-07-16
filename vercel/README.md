c95a586dbd6f48072cdd0404f8dcbaf8593b6ef7
Spenser Black <spenserblack01@gmail.com>
2022-07-16 10:17:59 -0400

    Initialize Svelte+TS project

---
+++ vercel/README.md
@@ -0,0 +1,11 @@
+# Svelte + TS + Vite
+
+## Development
+
+```bash
+# setup
+yarn
+
+# run server with hot reloading
+yarn dev
+```
---
+++ vercel/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Onefetch</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+</main>
