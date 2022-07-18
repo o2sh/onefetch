4ddabac0d968c44cf018ce56c09f49b74e5dff70
Spenser Black <spenserblack01@gmail.com>
2022-07-18 12:10:44 -0400

    Add some styling with a simple navbar

--- vercel/src/App.svelte
+++ vercel/src/App.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import Counter from './lib/Counter.svelte'
-  import getLanguages from './get-languages';
+  import Nav from './lib/Nav.svelte'
   import type { Languages } from './types';
-
-  let languages: Languages | null = null;
-
-  onMount(async () => {
-    languages = await getLanguages();
-  });
 </script>
 
 <main>
   <h1>Onefetch</h1>
 
   <div class="card">
-    <Counter />
+    <Nav active="/" />
   </div>
-  {#if languages}
-    <pre>{JSON.stringify(languages, null, 2)}</pre>
-  {:else}
-    <p>Getting languages</p>
-  {/if}
 </main>
---
+++ vercel/src/common.ts
@@ -0,0 +1,1 @@
+import 'sakura.css';
