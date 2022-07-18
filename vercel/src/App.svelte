53d597eff557493464c3e8b7b17da4a468ebf75d
Spenser Black <spenserblack01@gmail.com>
2022-07-18 10:50:18 -0400

    Add function to get language data
    Conditionally gets data based on environment. If in production, then it
    is gotten from the API, but if in development, it gets it from the local
    file.

--- vercel/src/App.svelte
+++ vercel/src/App.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import Counter from './lib/Counter.svelte'
+  import getLanguages from './get-languages';
+  import type { Languages } from './types';
+
+  let languages: Languages | null = null;
+
+  onMount(async () => {
+    languages = await getLanguages();
+  });
 </script>
 
 <main>
@@ -8,4 +17,9 @@
   <div class="card">
     <Counter />
   </div>
+  {#if languages}
+    <pre>{JSON.stringify(languages, null, 2)}</pre>
+  {:else}
+    <p>Getting languages</p>
+  {/if}
 </main>
--- vercel/vite.config.ts
+++ vercel/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import yaml from '@rollup/plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), yaml()],
 });
