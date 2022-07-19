f30ebedde18f5e386699d5b10fa391dcdc6f15bf
Spenser Black <spenserblack01@gmail.com>
2022-07-19 13:13:57 -0400

    Add installation instructions to homepage

--- vercel/src/Index.svelte
+++ vercel/src/Index.svelte
@@ -8,4 +8,24 @@
   <div class="card">
     <Nav active="/" />
   </div>
+  <div class="spacer"></div>
+  <p>
+    Get Onefetch from
+  </p>
+  <ul>
+    <li><a href="https://github.com/o2sh/onefetch/releases/latest">the releases page</a></li>
+    <li><a href="https://crates.io/crates/onefetch">crates.io</a> (<code>cargo install onefetch</code>)</li>
+    <li><a href="https://snapcraft.io/onefetch">snapcraft</a> (<code>sudo snap install onefetch</code>)</li>
+  </ul>
+  <p>
+    or view our
+    <a href="https://github.com/o2sh/onefetch/wiki/installation">installation guide</a>
+    for other installation methods or more details.
+  </p>
 </main>
+
+<style>
+  .spacer {
+    margin-top: 5rem;
+  }
+</style>
