1a11f9bd6371798b68c7315f48ca6ce791c7a713
Spenser Black <spenserblack01@gmail.com>
2022-07-20 17:54:18 -0400

    Set type of key parameter
    Co-authored-by: Ossama Hjaji <ossama-hjaji@live.fr>

--- vercel/api/languages/[key].ts
+++ vercel/api/languages/[key].ts
@@ -3,7 +3,7 @@ import type { VercelRequest as Request, VercelResponse as Response } from '@verc
 
 export default (request: Request, response: Response) => {
   response.setHeader('Content-Type', 'application/json');
-  const key = request.query.key;
+  const key = request.query.key as string;
   const language = data[key];
   if (language == null) {
     return response.status(404).send({ error: 'Language not found', key });
