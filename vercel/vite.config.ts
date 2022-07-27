63b7544da38e01449490a5f08d00e92cfd93573b
o2sh <ossama-hjaji@live.fr>
2022-07-27 22:11:27 +0200

    run prettier

--- vercel/vite.config.ts
+++ vercel/vite.config.ts
@@ -1,7 +1,7 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import yaml from '@rollup/plugin-yaml'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import yaml from '@rollup/plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        'main': resolve(__dirname, 'index.html'),
         'ascii-preview': resolve(__dirname, 'ascii-preview/index.html'),
       },
     },
   },
-})
+});
