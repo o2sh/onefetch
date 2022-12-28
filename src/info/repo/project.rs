f74c1a051ed4d2496c72ead51dbca13fa94125d1
o2sh <ossama-hjaji@live.fr>
2022-12-28 14:45:39 +0100

    add check for empty url in get_repo_name()

--- src/info/repo/project.rs
+++ src/info/repo/project.rs
@@ -40,6 +40,9 @@ impl ProjectInfo {
 }
 
 fn get_repo_name(repo_url: &str, manifest: Option<&Manifest>) -> Result<String> {
+    if repo_url.is_empty() {
+        return Ok(String::default());
+    }
     let url = git_repository::url::parse(repo_url.into())?;
     let path = git_repository::path::from_bstr(url.path.as_bstr());
     let repo_name = path
