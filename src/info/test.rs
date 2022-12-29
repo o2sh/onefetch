c5f72014e44298f14b64aaf46207fae7a1dbae21
Spenser Black <spenserblack01@gmail.com>
2022-12-29 01:44:33 +0000

    Bump git-testtools
    Fixes an issue where a test would open the editor if `tag.gpgSign` had
    been globally set in the environment where the tests are run.

--- src/info/test.rs
+++ src/info/test.rs
@@ -5,7 +5,7 @@ pub mod utils {
 
     pub fn repo(name: &str) -> Result<Repository> {
         let name = name.to_string();
-        let repo_path = git_testtools::scripted_fixture_repo_read_only(name).unwrap();
+        let repo_path = git_testtools::scripted_fixture_read_only(name).unwrap();
         let safe_repo = ThreadSafeRepository::open_opts(repo_path, open::Options::isolated())?;
         Ok(safe_repo.to_thread_local())
     }
