2e48f9ea31effb6c1583b61134836b8e8a2dc3c0
o2sh <ossama-hjaji@live.fr>
2024-03-17 00:32:14 +0100

    refacto pending info

--- src/info/pending.rs
+++ src/info/pending.rs
@@ -6,57 +6,65 @@ use serde::Serialize;
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PendingInfo {
-    pub pending_changes: String,
+    added: usize,
+    deleted: usize,
+    modified: usize,
 }
 
 impl PendingInfo {
     pub fn new(repo: &Repository) -> Result<Self> {
-        let pending_changes = get_pending_changes(repo)?;
-        Ok(Self { pending_changes })
+        let statuses = repo
+            .status(gix::progress::Discard)?
+            .dirwalk_options(|options| {
+                options.emit_untracked(gix::dir::walk::EmissionMode::Matching)
+            })
+            .into_index_worktree_iter(Vec::new())?;
+
+        let (added, deleted, modified) = statuses
+            .take_while(Result::is_ok)
+            .filter_map(Result::ok)
+            .filter_map(|item| item.summary())
+            .fold((0, 0, 0), |(added, deleted, modified), status| {
+                use gix::status::index_worktree::iter::Summary;
+                match status {
+                    Summary::Removed => (added, deleted + 1, modified),
+                    Summary::Added | Summary::Copied => (added + 1, deleted, modified),
+                    Summary::Modified | Summary::TypeChange => (added, deleted, modified + 1),
+                    Summary::Renamed => (added + 1, deleted + 1, modified),
+                    Summary::IntentToAdd | Summary::Conflict => (added, deleted, modified),
+                }
+            });
+        Ok(Self {
+            added,
+            deleted,
+            modified,
+        })
     }
 }
 
-fn get_pending_changes(repo: &Repository) -> Result<String> {
-    let statuses = repo
-        .status(gix::progress::Discard)?
-        .dirwalk_options(|options| options.emit_untracked(gix::dir::walk::EmissionMode::Matching))
-        .into_index_worktree_iter(Vec::new())?;
+impl std::fmt::Display for PendingInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut result = String::new();
+        if self.modified > 0 {
+            result = format!("{}+-", self.modified);
+        }
 
-    let (added, deleted, modified) = statuses
-        .take_while(Result::is_ok)
-        .filter_map(Result::ok)
-        .filter_map(|item| item.summary())
-        .fold((0, 0, 0), |(added, deleted, modified), status| {
-            use gix::status::index_worktree::iter::Summary;
-            match status {
-                Summary::Removed => (added, deleted + 1, modified),
-                Summary::Added | Summary::Copied => (added + 1, deleted, modified),
-                Summary::Modified | Summary::TypeChange => (added, deleted, modified + 1),
-                Summary::Renamed => (added + 1, deleted + 1, modified),
-                Summary::IntentToAdd | Summary::Conflict => (added, deleted, modified),
-            }
-        });
+        if self.added > 0 {
+            result = format!("{result} {}+", self.added);
+        }
 
-    let mut result = String::new();
-    if modified > 0 {
-        result = format!("{modified}+-");
+        if self.deleted > 0 {
+            result = format!("{result} {}-", self.deleted);
+        }
+
+        write!(f, "{}", result.trim())
     }
-
-    if added > 0 {
-        result = format!("{result} {added}+");
-    }
-
-    if deleted > 0 {
-        result = format!("{result} {deleted}-");
-    }
-
-    Ok(result.trim().into())
 }
 
 #[typetag::serialize]
 impl InfoField for PendingInfo {
     fn value(&self) -> String {
-        self.pending_changes.to_string()
+        self.to_string()
     }
 
     fn title(&self) -> String {
@@ -71,7 +79,9 @@ mod test {
     #[test]
     fn test_display_pending_info() {
         let pending_info = PendingInfo {
-            pending_changes: "4+-".to_string(),
+            added: 0,
+            deleted: 0,
+            modified: 4,
         };
 
         assert_eq!(pending_info.value(), "4+-".to_string());
