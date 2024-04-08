bb3e92ab958bf660618f920f683a6a30a0f7f2a3
Spenser Black <spenserblack01@gmail.com>
2024-04-08 19:01:03 +0000

    Analyze stats

--- src/info/langs/mod.rs
+++ src/info/langs/mod.rs
@@ -1,6 +1,9 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use gengo::{analysis, FileSource, Git, Builder};
+use gix::filter::plumbing::eol::Stats;
 use language::{Language, LanguageType};
 use std::collections::HashMap;
+use std::error::Error;
 use std::path::Path;
 use strum::IntoEnumIterator;
 
@@ -19,14 +22,17 @@ pub fn get_loc_by_language_sorted(
     language_types: &[LanguageType],
     include_hidden: bool,
 ) -> Result<Vec<(Language, usize)>> {
-    let stats = get_statistics(dir, globs_to_exclude, language_types, include_hidden);
+    let stats = match get_statistics(dir, globs_to_exclude, language_types, include_hidden) {
+        Ok(stats) => stats,
+        Err(e) => return Err(anyhow!("Could not analyze repository: {}", e))
+    };
 
-    let loc_by_language =
-        get_loc_by_language(&stats).context("Could not find any source code in this repository")?;
+    // NOTE If finer control is ever needed, summary_with can be used.
+    let mut size_by_language: Vec<(Language, _)> = stats.summary().iter().filter_map(|(lang, size)| (*lang).try_into().map(|l| (l, *size)).ok()).collect();
+    // NOTE Sort by size (descending) first, then by language name (ascending) in case the size is equal
+    size_by_language.sort_by_key(|(language, size)| (usize::MAX - *size, language.to_string()));
 
-    let loc_by_language_sorted = sort_by_loc(loc_by_language);
-
-    Ok(loc_by_language_sorted)
+    Ok(size_by_language)
 }
 
 fn sort_by_loc(map: HashMap<Language, usize>) -> Vec<(Language, usize)> {
@@ -67,9 +73,11 @@ fn get_statistics(
     globs_to_exclude: &[String],
     language_types: &[LanguageType],
     include_hidden: bool,
-) -> ! {
+) -> Result<gengo::Analysis, Box<dyn Error>> {
     // TODO Determine best way to ignore files (and if that should continue to be handled by onefetch)
-    todo!("Get statistics")
+    let file_source = Git::new(dir, "HEAD")?;
+    let gengo = Builder::new(file_source).build()?;
+    gengo.analyze()
 }
 
 fn filter_languages_on_type(types: &[LanguageType]) -> ! {
