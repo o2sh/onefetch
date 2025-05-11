b0949b87cdd9eabb111651079732eff414aeb610
Sk7Str1p3 <finenkofedor@gmail.com>
2025-05-11 11:28:56 +0000

    config: implement r/w functions

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,7 @@
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
+
 use crate::{
     cli::{NumberSeparator, When},
     info::utils::info_field::InfoType,
@@ -51,5 +54,50 @@ impl Default for ConfigOptions {
             true_color: When::Auto,
             nerd_fonts: false,
         }
+    }
+}
+
+impl ConfigOptions {
+    pub fn read<P>(path: P) -> Self
+    // Is it even should panic?
+    where
+        P: AsRef<Path>,
+    {
+        let contents = fs::read_to_string(path);
+        match contents {
+            Err(_) => Self::default(),
+            Ok(contents) => {
+                // I wish to print exact error here, like syntax errors
+                toml::from_str(&contents).unwrap()
+            }
+        }
+    }
+
+    pub fn write<P>(self, path: P) -> Result<()>
+    // I believe user would like to generate config with CLI flags
+    // I mean to write disabled_fields with --disabled-fields flag
+    where
+        P: AsRef<Path>,
+    {
+        // I dont think this can panic so i simply unwrapped it
+        let contents = toml::to_string(&self).unwrap();
+        match fs::create_dir_all(&path.as_ref().parent().unwrap_or(Path::new("/"))) {
+            Ok(_) => match fs::write(&path, &contents) {
+                Ok(_) => {
+                    let path = path.as_ref().display();
+                    println!("Config config file created at: {path}")
+                }
+                Err(e) => {
+                    let path = path.as_ref().display();
+                    eprintln!("Failed to write config file at {path}: {e}")
+                }
+            },
+            Err(e) => {
+                let path = path.as_ref().display();
+                eprintln!("Failed to create config directory {path}: {e}");
+            }
+        }
+        // Im not sure it should return simple Ok(())?
+        Ok(())
     }
 }
